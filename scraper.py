f2acaeb93cac2f2a48c08e470fe88d659621d6d8
1hef001 <ashwins1211@gmail.com>
2020-05-17 15:07:42 +0530

    issues #6 & #7 are resolved

--- scraper.py
+++ scraper.py
@@ -1,30 +1,21 @@
 from newspaper import Article
+from newspaper import ArticleException
 import newspaper
-import requests
-from bs4 import BeautifulSoup
+import string
 
-def scrape_google_links(url='https://www.news.google.com'):
+
+def scrape_news_links(url):
     '''
         Scrapes links : not only google but any online vendor.
         set url while calling the function
     '''
     print('scraping_links')
-    paper = newspaper.build(url)
+    paper = newspaper.build(url, memoize_articles=False)
     links = []
     for article in paper.articles:
         links.append(article.url)
-    # resp = requests.get(url)
-    # soup = BeautifulSoup(resp.text, 'lxml')
-    # g_links = soup.find_all('a')
-    # links = []
-    # for link in g_links:
-    #     l = link.get('href')
-    #     if l == None:
-    #         continue
-    #     elif l.startswith('./articles'):
-    #         uri = url + l.split('.')[1]
-    #         if uri not in links:
-    #             links.append(uri)
+    
+    # print(links)
     return links
 
 def clean_text(text):
@@ -32,6 +23,10 @@ def clean_text(text):
         To clean text
     '''
     print('cleaning_text')
+    # text = text.strip()
+    # text = text.lower()
+    # for punct in string.punctuation:
+    #     text = text.replace(punct, '')
     string = text.split('\n')
     text = " ".join(string)
     text.replace('\\', '')
@@ -43,25 +38,29 @@ def get_content(links):
     '''
     print('getting content')
     content = []
-    for url in links:
-        article = Article(url)
-        article.download()
-        article.parse()
-        title = article.title
-        news = clean_text(article.text)
-        if title != None or news != None or news != ' ' or news != '':      # for sites which news content cannot be scraped
-            content.append([title, news])
-    
-    return content
+    try:
+        for url in links:
+            article = Article(url, language='en')
+            article.download()
+            article.parse()
+            title = clean_text(article.title)
+            news = clean_text(article.text)
+            if title != None or news != None or news != ' ' or news != '':      # for sites which news content cannot be scraped
+                content.append([title, news])
+    except ArticleException as ae:
+        if 'Article \'download()\' failed' in ae:
+            print('Could not get article')
+    finally:    
+        return content
     
 
-def scraper():
+def scraper(link='https://timesofindia.indiatimes.com/'):
     '''
         aggregator function
     '''
     print('scraper_main')
-    return get_content(scrape_google_links()[:100])
+    return get_content(scrape_news_links(link)[:100])
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # links = scrape_google_links()
     # print(get_content(links[:15]))
